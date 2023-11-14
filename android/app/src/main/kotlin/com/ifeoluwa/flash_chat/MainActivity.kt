48d18cd239a46c7aad81313d1264b4746470ec4c
Precious <preciouspreccy325@gmail.com>
2023-11-15 00:10:59 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/ifeoluwa/flash_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ifeoluwa.flash_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,110 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flash_chat/components/rounded_button.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'chat_screen.dart';
+
+
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
+  String? email;
+  String? password;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    await _auth.signInWithEmailAndPassword(
+                        email: email!, password: password!);
+                  } catch (e) {
+                    print(e);
+                  }
+                  _auth.authStateChanges().listen((User? user) {
+                    if (user == null) {
+                      print('User is currently signed out!');
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } else {
+                      setState(() {
+                        showSpinner = false;
+                      });
+                      print('User is signed in!');
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  });
+                },
+                title: 'Log In',
+                colour: Colors.lightBlueAccent,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
